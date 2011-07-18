1a17aaebe42f0760a6280951d7f038f08e1a4305
nerdsrescueme <nerdsrescueme@gmail.com>
2011-07-17 21:11:48 -0400

    Small fix to blog index routes.

--- config/routes.php
+++ config/routes.php
@@ -1,8 +1,11 @@
 <?php
 
 return array(
+	'blog/index' => 'blog',
+	'blog/index/(:any)' => 'blog/index/$1',
 	'blog/tag/(:any)' => 'blog/tag/$1',
 	'blog/view/(:any)' => 'blog/view/$1',
+
 	'blog/(:any)' => 'blog/view/$1',
 );
 
